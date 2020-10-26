37cf3df2a14e0b24b5dabd2ccb15a17d17483300
Oleg Arkhangelsky <radium49@mail.ru>
2020-10-26 08:37:34 +0300

    Name added to GenericObjectFactory for debugging purposes

--- FirstEngine/GenericObjectFactory.h
+++ FirstEngine/GenericObjectFactory.h
@@ -1,31 +1,53 @@
 #pragma once
 
 #include <map>
+#include <string>
 
 template <class BaseClass, class IdType>
 class GenericObjectFactory {
 
-    std::map<IdType, BaseClass* (*)()> m_creationFunctions;
+	std::map<IdType, BaseClass* (*)()> m_creationFunctions;
+	std::map<IdType, std::string> m_names;
 
 public:
-    template <class SubClass>
-    bool Register(IdType id) {
-        auto findIt = m_creationFunctions.find(id);
-        if (findIt == m_creationFunctions.end()) {
-            m_creationFunctions[id] = []()->BaseClass* { return new SubClass; };
-            return true;
-        }
+	template <class SubClass>
+	bool Register(IdType id) {
+		auto findIt = m_creationFunctions.find(id);
+		if (findIt == m_creationFunctions.end()) {
+			m_creationFunctions[id] = []()->BaseClass* { return new SubClass; };
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    BaseClass* Create(IdType id) {
-        auto findIt = m_creationFunctions.find(id);
-        if (findIt != m_creationFunctions.end()) {
-            auto pFunc = findIt->second;
-            return pFunc();
-        }
+	template <class SubClass>
+	bool Register(IdType id, const std::string& name) {
+		auto findIt = m_creationFunctions.find(id);
+		if (findIt == m_creationFunctions.end()) {
+			m_creationFunctions[id] = []()->BaseClass* { return new SubClass; };
+			m_names[id] = name;
+			return true;
+		}
 
-        return NULL;
-    }
+		return false;
+	}
+
+	BaseClass* Create(IdType id) {
+		auto findIt = m_creationFunctions.find(id);
+		if (findIt != m_creationFunctions.end()) {
+			auto pFunc = findIt->second;
+			return pFunc();
+		}
+
+		return nullptr;
+	}
+
+	const std::string& GetName(IdType id) {
+		auto findIt = m_names.find(id);
+		if (findIt != m_names.end()) {
+			return findIt->second;
+		}
+		return "No name or not registered";
+	}
 };
